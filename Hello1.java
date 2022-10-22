c5c98eb1f1eb334388b26164ad9763b40a61dadb
CDACShubham <114065052+CDACShubham@users.noreply.github.com>
2022-10-22 23:20:03 +0530

    Delete Hello1.java

--- Hello1.java
+++
@@ -1,7 +0,0 @@
-class Hello1{
-    public static void main(String args[]){
-        int a,b;
-        a=10;b=20;
-        int c = a+b;
-System.out.println("Addition of "+a+" and "+b+" number is = "+c);
-}}
